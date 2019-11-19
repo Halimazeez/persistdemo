71bd878bc2b0966cd7d2cfad01944746690b30c2
Halim Azeez <halim.azeez@signaturelnd.com>
2019-11-19 10:08:33 +0000

    persist store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,20 @@
 import { applyMiddleware, createStore, combineReducers } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
 import promise from "redux-promise-middleware";
 import { composeWithDevTools } from "redux-devtools-extension";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
 import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
 
 const enhancer = composeWithDevTools(applyMiddleware(thunk, promise));
 
-export const store = createStore(rootReducer, enhancer);
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, enhancer);
+export const persistor = persistStore(store);
