11643ec9b5e3bca4e93b7c0ec6f978a040933e69
Halim Azeez <halim.azeez@signaturelnd.com>
2019-11-19 14:31:10 +0000

    Updates

---
+++ precache-manifest.4ac81935fa93746985f698e7a40bf282.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930bd321c493d72a35ccfe4a03e68155",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1907c297cf40ba66e9f",
+    "url": "/static/css/main.bd36bde1.chunk.css"
+  },
+  {
+    "revision": "4d6204abb1035345f066",
+    "url": "/static/js/2.da762fd3.chunk.js"
+  },
+  {
+    "revision": "b1907c297cf40ba66e9f",
+    "url": "/static/js/main.fac0b7ff.chunk.js"
+  },
+  {
+    "revision": "01685684fcadcdb0af4c",
+    "url": "/static/js/runtime-main.a6015a0d.js"
+  }
+]);
